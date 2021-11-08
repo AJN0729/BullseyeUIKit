036a786808ffb8e53c88d1f1061dcbf7a50696f9
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-08 10:39:08 -0600

    slider and message are working.

--- BullseyeUIKit/ViewController.swift
+++ BullseyeUIKit/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 class ViewController: UIViewController {
     
     var currentValue = 50
+    var targetValue = 0
 
+    @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
+        startNewRound()
     }
 
     @IBAction func showAlert() {
         
-        let message = "the value of the slider is now: \(currentValue)"
+        let message = "the value of the slider is now: \(currentValue)" + "\nThe target value is: \(targetValue)"
       
         let alert = UIAlertController(title: "Hello World", message: message, preferredStyle: .alert)
         
@@ -28,12 +32,19 @@ class ViewController: UIViewController {
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+
+        startNewRound()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         let roundedValue = slider.value.rounded()
         currentValue = Int(roundedValue)
     }
-    
+
+    func startNewRound() {
+        targetValue = Int.random(in: 1...100)
+        currentValue = 50
+        slider.value = Float(currentValue)
+    }
 }
 
