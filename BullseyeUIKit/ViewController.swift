cedb1b2da6db5dc7c5c4eded47f6fc501188fdf7
AJN0729 <andrewn0729@student.k12.tulsaschools.org>
2021-11-08 09:09:38 -0600

    stuff is working in places, hit me button in wrong place

--- BullseyeUIKit/ViewController.swift
+++ BullseyeUIKit/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func showAlert() {
+      
+        let alert = UIAlertController(title: "Hello World", message: "Hi", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
